9e44d936b56d9f13a5aa65595d0e7cb46f10b287
Sergey Oleynik <remember664@gmail.com>
2013-12-06 15:51:26 +0200

    Update AssemblyInfo.cs

--- Sitecore.RestSharp/Properties/AssemblyInfo.cs
+++ Sitecore.RestSharp/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.RestSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sitecore mapper for RestSharp. GitHub: https://github.com/remember664/Sitecore.RestSharp")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sitecore.RestSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Sergey Oleynik 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
